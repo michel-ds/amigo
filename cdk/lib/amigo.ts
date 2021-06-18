5a4d6537e00ed19c4970eca0af196caabdb08532
Akash Askoolum <akash1810@gmail.com>
2021-06-18 09:02:00 +0100

    feat: Validate the tags in the YAML template
    CDK sorts tags alphabetically.
    
    Any difference in a resource's tags will result in the resource appearing in the change set.
    This can be noisy.
    
    In order to reduce the noise, add an error annotation when the expected and actual tags differ.
    By default, an error annotation will prevent the stack from synthing and so CI should fail.
    
    Example output:
    
    ```console
    $ cdk synth --path-metadata false --version-reporting false
    [Error at /AMIgo/YamlTemplate/RootRole] TAGGING ERROR. Missing identity tags or tags not listed in correct order. Expected: [App, Stack, Stage]. Actual: []
    [Error at /AMIgo/YamlTemplate/AmigoDataBucket] TAGGING ERROR. Missing identity tags or tags not listed in correct order. Expected: [App, Stack, Stage]. Actual: [Stage, Stack, App]
    [Error at /AMIgo/YamlTemplate/LoadBalancerSecurityGroup] TAGGING ERROR. Missing identity tags or tags not listed in correct order. Expected: [App, Stack, Stage]. Actual: []
    [Error at /AMIgo/YamlTemplate/LoadBalancer] TAGGING ERROR. Missing identity tags or tags not listed in correct order. Expected: [App, Stack, Stage]. Actual: []
    [Error at /AMIgo/YamlTemplate/ApplicationSecurityGroup] TAGGING ERROR. Missing identity tags or tags not listed in correct order. Expected: [App, Stack, Stage]. Actual: []
    [Error at /AMIgo/YamlTemplate/AutoscalingGroup] TAGGING ERROR. Missing identity tags or tags not listed in correct order. Expected: [App, Stack, Stage]. Actual: [Stage, Stack, App]
    [Error at /AMIgo/YamlTemplate/PackerSecurityGroup] TAGGING ERROR. Missing identity tags or tags not listed in correct order. Expected: [App, Stack, Stage]. Actual: []
    Found errors
    ```
    
    See:
      - https://github.com/aws/aws-cdk/blob/b2037d3b1a63715f71587681c84a5fd34be939a7/packages/%40aws-cdk/core/lib/tag-manager.ts#L288-L293

--- cdk/lib/amigo.ts
+++ cdk/lib/amigo.ts
@@ -1,20 +1,80 @@
+import { readFileSync } from "fs";
 import path from "path";
 import { CfnInclude } from "@aws-cdk/cloudformation-include";
-import type { App } from "@aws-cdk/core";
+import type { CfnIncludeProps } from "@aws-cdk/cloudformation-include/lib/cfn-include";
+import type { App, IConstruct } from "@aws-cdk/core";
+import { Annotations, TagManager } from "@aws-cdk/core";
 import type {
   GuStackProps,
   GuStageParameter,
 } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
+import { yamlParse } from "yaml-cfn";
 
-const yamlTemplateFilePath = path.join(__dirname, "../../cloudformation.yaml");
+const areArraysEqual = <T>(arrayOne: T[], arrayTwo: T[]) => {
+  return (
+    arrayOne.length === arrayTwo.length &&
+    arrayOne.every((value, index) => value === arrayTwo[index])
+  );
+};
+
+interface CfnYaml {
+  Resources: Record<string, { Properties: { Tags?: Array<{ Key: string }> } }>;
+}
+
+class GuYamlMigration extends CfnInclude {
+  constructor(stack: GuStack, props: CfnIncludeProps) {
+    super(stack, "YamlTemplate", props);
+
+    const template = yamlParse(
+      readFileSync(props.templateFile, "utf8")
+    ) as CfnYaml;
+
+    this.node.findAll().forEach((construct: IConstruct) => {
+      if (TagManager.isTaggable(construct)) {
+        const identityTags = ["App", "Stack", "Stage"];
+
+        const constructId = construct.node.id;
+
+        const currentTags = (
+          template.Resources[constructId].Properties.Tags ?? []
+        ).map((_) => _.Key);
+
+        const expectedTags = Array.from(
+          new Set([...identityTags, ...currentTags])
+        );
+        const expectedTagsInOrder = [...expectedTags].sort();
+
+        const isTagOrderCorrect = areArraysEqual<string>(
+          expectedTags,
+          expectedTagsInOrder
+        );
+
+        const hasMissingTags =
+          expectedTags.filter(
+            (identityTag) => !currentTags.includes(identityTag)
+          ).length > 0;
+
+        if (hasMissingTags || !isTagOrderCorrect) {
+          Annotations.of(construct).addError(
+            `TAGGING ERROR. Missing identity tags or tags not listed in correct order. Expected: [${expectedTagsInOrder.join(
+              ", "
+            )}]. Actual: [${currentTags.join(", ")}]`
+          );
+        }
+      }
+    });
+  }
+}
 
 export class AmigoStack extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
 
-    new CfnInclude(this, "YamlTemplate", {
-      templateFile: yamlTemplateFilePath,
+    const templateFile = path.join(__dirname, "../../cloudformation.yaml");
+
+    new GuYamlMigration(this, {
+      templateFile,
       parameters: {
         Stage: this.getParam<GuStageParameter>("Stage"), // TODO `GuStageParameter` could be a singleton to simplify this
       },
